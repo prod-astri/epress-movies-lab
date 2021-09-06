305d22d0380a88c8b3c69a906b54edd72b7449c9
prod.astri <58519454+prod-astri@users.noreply.github.com>
2021-09-06 16:11:52 +0200

    add new celebrities page

--- routes/celebrities.js
+++ routes/celebrities.js
@@ -5,12 +5,37 @@ router.get("/celebrities", (req, res, next) => {
   Celebrity.find()
 	.then(celebritiesFromDB => {
 		console.log(celebritiesFromDB);
-		res.render('books', { celebritiesList: celebritiesFromDB });
+    //// how does THIS work???
+    //           V should it not be celebrities / index??
+		res.render('celebrities', { celebritiesList: celebritiesFromDB });
 	})
 	.catch(err => {
 		
 		next(err);
 	})
+});
+
+
+
+router.get("/celebrities/new", (req, res, next) => {
+  res.render('celebrities/new');
+  
+});
+
+router.get("/celebrities/:id", (req, res, next) => {
+  console.log(req.params)
+  const celebId = req.params.id
+  Celebrity.findById(celebId)
+  .then(celebrityFromDB => {
+    console.log(celebrityFromDB);
+    //// how does THIS work???
+    //           V should it not be celebrities / index??
+    res.render('celebrities/show', { celebrity: celebrityFromDB });
+  })
+  .catch(err => {
+    
+    next(err);
+  })
 });
 
 module.exports = router;
