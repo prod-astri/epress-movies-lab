4511772a4dbe9986c87f17597ba10423ece37f7a
prod.astri <58519454+prod-astri@users.noreply.github.com>
2021-09-06 17:18:45 +0200

    done pt-1 (iterations 1-6)

--- routes/celebrities.js
+++ routes/celebrities.js
@@ -3,19 +3,17 @@ const Celebrity = require('../models/Celebrity');
 
 router.get("/celebrities", (req, res, next) => {
   Celebrity.find()
-	.then(celebritiesFromDB => {
-		console.log(celebritiesFromDB);
+  .then(celebritiesFromDB => {
+    console.log(celebritiesFromDB);
     //// how does THIS work???
     //           V should it not be celebrities / index??
-		res.render('celebrities', { celebritiesList: celebritiesFromDB });
-	})
-	.catch(err => {
-		
-		next(err);
-	})
+    res.render('celebrities', { celebritiesList: celebritiesFromDB });
+  })
+  .catch(err => {
+    
+    next(err);
+  })
 });
-
-
 
 router.get("/celebrities/new", (req, res, next) => {
   res.render('celebrities/new');
@@ -41,18 +39,54 @@ router.get("/celebrities/:id", (req, res, next) => {
 router.post("/celebrities", (req, res, next) => {
   const {name, occupation, catchphrase} = req.body;
   Celebrity.create({
-		name: name,
-		occupation: occupation,
-		catchphrase: catchphrase
-	})
-	.then(newCeleb => {
-		console.log(newCeleb);
-		// redirect / render the detail view for this book
-		//res.render('bookDetails', { book: createdBook });
-		// doesnt get arguments cause its like writing it directly in the browser
-		// redirects to the new page instead of rendering the book I had
-		res.redirect(`/celebrities/${newCeleb._id}`);
-	})
-	.catch(err => next(err));
+    name: name,
+    occupation: occupation,
+    catchphrase: catchphrase
+  })
+  .then(newCeleb => {
+    console.log(newCeleb);
+    res.redirect(`/celebrities/${newCeleb._id}`);
+  })
+  .catch(err => next(err));
 });
+
+router.get('/celebrities/:id/delete', (req, res, next) => {
+  console.log('deleting this celebrity');
+  const celebId = req.params.id;
+  Celebrity.findByIdAndDelete(celebId)
+  .then(() => {
+    res.redirect('/celebrities')
+  })
+  .catch(err => {
+    next(err);
+  })
+});
+
+router.get('/celebrities/:id/edit', (req, res, next) => {
+  console.log('editing this celebrity');
+  const celebId = req.params.id;
+  Celebrity.findById(celebId)
+  .then(celeb => {
+    console.log(celeb);
+    res.render('celebrities/edit', { celebrity: celeb });
+  }).catch(err => {
+    next(err);
+  })
+});
+
+router.post("/celebrities/:id", (req, res, next) => {
+  const {name, occupation, catchphrase} = req.body;
+  const celebId = req.params.id;
+  Celebrity.findByIdAndUpdate(celebId, {
+    name: name,
+    occupation: occupation,
+    catchphrase: catchphrase
+  }, { new: true })
+  .then(newCeleb => {
+    console.log(newCeleb);
+    res.redirect(`/celebrities/${newCeleb._id}`);
+  })
+  .catch(err => next(err));
+});
+
 module.exports = router;
